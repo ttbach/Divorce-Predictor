95617ab26e95b1d8e9a1f4340c59bea166905914
ttbach <poohthao@yahoo.co.uk>
2016-12-13 09:51:45 -0500

    first commit

---
+++ dictionary.py
@@ -0,0 +1,33 @@
+dictionary = {
+    # Race
+    'r0': 'White Alone',
+    'r1': 'White Alone, non-Hispanic',
+    'r2': 'Black Alone',
+    'r3': 'American Indian and Alaska Native alone',
+    'r4': 'Asian alone',
+    'r5': 'Native Hawaiian and Other Pacific Islander alone',
+    'r6': 'Some other race alone',
+    'r7': 'Two or more races',
+    'r8': 'Hispanic (of any race)',
+    # Nativity
+    'n0': 'Native',
+    'n1': 'Foreign Born',
+    # Educational Attainment
+    'ed0': 'Less than high school',
+    'ed1': 'High School Graduate',
+    'ed2': 'Some college or associate\'s degree',
+    'ed3': 'Bachelor\'s degree or more',
+    #Employment Status
+    'em0': 'Employed',
+    'em1': 'Unemployed',
+    'em2': 'Not in labor force',
+    #Income
+    'i0': 'Less than $25,000',
+    'i1': '$25,000 to $49,999',
+    'i2': '$50,000 to $74,999',
+    'i3': '$75,000 to $99,999',
+    'i4': '$100,000 and over',
+    #Tenure
+    't0': 'Owns home',
+    't1': 'Rents home'
+    }
